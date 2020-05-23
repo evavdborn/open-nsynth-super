f8483e82983b934db132bac5d539fc0cda8c060d
evavdborn <43637609+evavdborn@users.noreply.github.com>
2020-05-23 17:47:07 +0200

    included all the samples I created myself

--- audio/workdir/helpers/convert_aif_to_wav.py
+++ audio/workdir/helpers/convert_aif_to_wav.py
@@ -1,8 +1,17 @@
 #	Converts AIF input files to WAV
 
-import os, subprocess
+import os
+import subprocess
 
-for fname in os.listdir('.'):
+path = os.getcwd() 
+print(path)
+# In Windows this is the root path is different: We will change it to the right path
+os.chdir("C:\\Users\\s157874\\Documents\\GitHub\\open-nsynth-super\\audio\\workdir\\helpers\\sample")
+path = os.getcwd()
+print(path)
+
+for fname in os.listdir(path):
 	nfn = fname.replace('aif', 'wav')
-	subprocess.call(["sox", fname, "-b", "16", "-r", "16000", "-c", "1", nfn])
-	os.remove(fname)
+	print(os.listdir(path))
+	subprocess.call(["sox", fname, "-b", "16", "-r", "16000", "-c", "1", nfn], shell=True)
+	# os.remove(fname)
